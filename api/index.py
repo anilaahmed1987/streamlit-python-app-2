6a06a7cd8d3087be0723974942192a6259b126fd
anilaahmed1987 <anilaahmed1987@gmail.com>
2025-02-20 12:39:24 +0500

    second changes

--- api/index.py
+++ api/index.py
@@ -4,4 +4,4 @@ from streamlit import config
 def handler(event, context):
     config.set_option('server.address', '0.0.0.0')
     config.set_option('server.port', 8501)
-    bootstrap.run('../app.py', '', [], {}) 
+    bootstrap.run('../streamlit_app.py', '', [], {}) 
